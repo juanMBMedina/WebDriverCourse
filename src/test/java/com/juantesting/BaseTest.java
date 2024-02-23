40f8bfd35cbfe9eae6129608d0abd2191a888e77
Juan Miguel Blanco Medina <juanmblancom@gmail.com>
2024-02-22 22:10:23 -0500

    Ends Chapter 3

--- src/test/java/com/juantesting/BaseTest.java
+++ src/test/java/com/juantesting/BaseTest.java
@@ -1,8 +1,12 @@
 package com.juantesting;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 import static com.juantesting.constants.ChromeDriverSettings.CHROME_BINARIES_PATH;
 import static com.juantesting.constants.ChromeDriverSettings.CHROME_DRIVER_PATH;
@@ -10,10 +14,13 @@ import static com.juantesting.constants.ChromeDriverSettings.CHROME_DRIVER_PATH;
 public class BaseTest {
 
     private WebDriver driver;
+    private String urlOfCourse = "https://the-internet.herokuapp.com/";
 
     public static void main(String args[]) {
         BaseTest test = new BaseTest();
         test.setUp();
+        test.courseClassMethod();
+        // test.exerciseMethod();
     }
 
     public void setUp() {
@@ -21,8 +28,27 @@ public class BaseTest {
         ChromeOptions co = new ChromeOptions();
         co.setBinary(CHROME_BINARIES_PATH);
         driver = new ChromeDriver(co);
-        driver.get("https://the-internet.herokuapp.com/");
-        System.out.println(driver.getTitle());
+        driver.manage().window().maximize();
+    }
+
+    public void courseClassMethod() {
+        driver.get(urlOfCourse);
+        // Get all Elements with Tag
+        String strTag = "a";
+        List<WebElement> listLinks = driver.findElements(By.tagName(strTag));
+        System.out.println("You have " + listLinks.size() + " elements with tag name: " + strTag);
+        // Select Web element with Click
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs!"));
+        inputsLink.click();
+    }
+
+    public void exerciseMethod() {
+        String strTagName = "Qes";
+        driver.get(urlOfCourse);
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> listLiInPage = driver.findElements(By.tagName(strTagName));
+        System.out.println("You have (" + listLiInPage.size() + ") elements with tag name: " + strTagName);
     }
 
 }
